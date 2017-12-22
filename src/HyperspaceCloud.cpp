bd6fc4f8be5b170e4c6f313c54306c5e628cd2dd
John Bartholomew <jpa.bartholomew@gmail.com>
2017-12-22 20:45:44 +0000

    Move JsonUtils.{h,cpp} to src/serialization/
    Warning: Visual Studio project not updated in this commit.
    
    JsonUtils is largely Pioneer-specific code and pulls in Pioneer headers.
    It's not part of the JSON library codebase, so doesn't really fit in contrib/

--- src/HyperspaceCloud.cpp
+++ src/HyperspaceCloud.cpp
@@ -15,7 +15,7 @@
 #include "graphics/Renderer.h"
 #include "graphics/VertexArray.h"
 #include "graphics/RenderState.h"
-#include "json/JsonUtils.h"
+#include "serialization/JsonUtils.h"
 #include "GameSaveError.h"
 
 using namespace Graphics;
